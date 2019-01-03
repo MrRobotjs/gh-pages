218f41869a94d880df12d099ae9ceedf41f520e4
Christopher <christopher.00.ramos@gmail.com>
2019-01-03 09:06:25 -0800

    Removed netlify deploys from analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,15 @@ const contentful = require('contentful');
 const manifestConfig = require('./manifest-config');
 require('dotenv').config();
 
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = 'https://christopher-portfolio.netlify.com',
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === 'production';
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
 const { ACCESS_TOKEN, SPACE_ID } = process.env;
 
 const client = contentful.createClient({
@@ -25,6 +34,29 @@ module.exports = client.getEntries().then(entries => {
         options: {
         // replace "UA-XXXXXXXXX-X" with your own Tracking ID
           trackingId: "UA-131673013-1",
+        },
+      },
+      {
+        resolve: 'gatsby-plugin-robots-txt',
+        options: {
+          host: "https://christopher-portfolio.netlify.com",
+          sitemap: "https://christopher-portfolio.netlify.com/sitemap.xml",
+          resolveEnv: () => NETLIFY_ENV,
+          env: {
+            production: {
+              policy: [{ userAgent: '*' }]
+            },
+            'branch-deploy': {
+              policy: [{ userAgent: '*', disallow: ['/'] }],
+              sitemap: null,
+              host: null
+            },
+            'deploy-preview': {
+              policy: [{ userAgent: '*', disallow: ['/'] }],
+              sitemap: null,
+              host: null
+            },
+          },
         },
       },
       {
