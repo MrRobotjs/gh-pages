6e7fb7bec629aedf10a9946b2d8c2534e092fddb
Christopher <christopher.00.ramos@gmail.com>
2019-01-04 12:35:40 -0800

    Changes

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['airbnb', 'prettier'],
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    'react/require-default-props': 'off',
-  },
-  parser: 'babel-eslint',
-};
